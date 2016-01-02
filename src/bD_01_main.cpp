e8f277828c4570c4347c534b66794034ee6643cc
Fisica2014-FF <directoryh22@gmail.com>
2016-01-03 00:38:40 +0100

    Correzioni minori e commenti aggiunti.

--- src/bD_01_main.cpp
+++ src/bD_01_main.cpp
@@ -22,12 +22,15 @@
 #define DBG(x, y) y
 #endif
 
+
+// Quite bad function because it has to be called in a certain order
+// from the main...
 unsigned int read(std::ifstream& ifs, int energies[]) {
 	using namespace std;
 	unsigned int numpoints = 0;
 
+	// (Try to) read from file
 	ifs >> numpoints;
-
 	for (unsigned int i = 0; i < numpoints; ++i) {
 		ifs >> energies[i];
 	}
@@ -38,29 +41,34 @@ unsigned int read(std::ifstream& ifs, int energies[]) {
 void dump(unsigned int evID, unsigned int numpoints, int energies[]) {
 	using namespace std;
 
+	//If numpoints is 0 then the file has ended/is invalid.
 	if (numpoints == 0)
 		return;
 
-	cout << "Event " << evID << ": " << numpoints << "points" << endl;
+	cout << "Event " << evID << ": " << numpoints << " points" << endl;
 	for (unsigned int i = 0; i < numpoints; ++i) {
 		cout << "    " << energies[i] << endl;
 	}
 	return;
 }
 
-// PROGETTO PENSATO PER SALVARE TUTTE LE VARIE OPZIONI POSSIBILI
 int main(int argc, char* argv[]) {
 
 	using namespace std;
 
+	//Read file name.
 	DBG(const char* filename = "./bragg_events.txt";
 		, const char* filename = argv[1];)
 
+	//Open file
 	ifstream ifs(filename);
+
+	//Create variables.
 	int energies[128];
 	unsigned int numpoints;
 	unsigned int evID;
 
+	//Dump every line of the file
 	while (ifs) {
 		ifs >> evID;
 		numpoints = read(ifs, energies);
